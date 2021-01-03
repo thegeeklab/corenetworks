c0fef0db703ddb5c245e405a7dea49b7e92b2806
Robert Kaussow <mail@geeklabor.de>
2021-01-03 14:34:02 +0100

    cleanup __init__

--- corenetworks/__init__.py
+++ corenetworks/__init__.py
@@ -3,10 +3,4 @@
 
 from corenetworks.client import CoreNetworks  # noqa
 
-__author__ = "Robert Kaussow"
-__project__ = "corenetworks"
-__license__ = "MIT"
-__maintainer__ = "Robert Kaussow"
-__email__ = "mail@thegeeklab.de"
-__url__ = "https://github.com/thegeeklab/corenetworks"
-__version__ = "0.1.6"
+__version__ = "0.0.0"
